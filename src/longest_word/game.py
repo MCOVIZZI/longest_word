60af7e5bbb5f59de3da52b7783ee32d8fe020414
Mauricio Covizzi <mauriciocovizzi@gmail.com>
2025-07-31 10:40:19 +0000

    Arreglando los problemas

--- src/longest_word/game.py
+++ src/longest_word/game.py
@@ -1,14 +1,19 @@
-# game.py
 
-# [...]
+import string
+import random
 
-def is_valid(self, word):
-    if not word:
-        return False
-    letters = self.grid.copy() # Consume letters from the grid
-    for letter in word:
-        if letter in letters:
-            letters.remove(letter)
-        else:
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+    def is_valid(self, word):
+        if not word:
             return False
-    return True
+        letters = self.grid.copy() # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
