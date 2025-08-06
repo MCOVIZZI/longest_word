9c431590f0de0e8b41b87ba56ff69d9ca835d95e
Mauricio Covizzi <mauriciocovizzi@gmail.com>
2025-08-06 21:21:13 +0000

    Finally discovered how this shit works

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask
+from flask import Flask, render_template, request
 from src.longest_word.game import Game
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    game = Game()
+    return render_template('home.html', grid=game.grid)
+
 
 @app.route('/check', methods=["POST"])
 def check():
